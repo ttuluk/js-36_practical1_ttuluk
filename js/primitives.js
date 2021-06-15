fd08c3a5131b412b2026989e904865bdf965f651
ttuluk <ttasya.k@gmail.com>
2021-06-15 23:41:02 +0300

    Create primitives.js

---
+++ js/primitives.js
@@ -0,0 +1,51 @@
+console.log('primitives');
+
+//STRING   
+let stringData = 'string data';
+console.log(stringData);
+//typeof - оператор определения типа данных
+console.log(typeof stringData);
+
+stringData = '';
+console.log(stringData);
+console.log(typeof stringData);
+
+stringData = ' ';
+console.log(stringData);
+console.log(typeof stringData);
+
+//NUMBER    
+let number = 1;
+console.log(number);
+console.log(typeof number);
+
+number = 2e53;
+console.log(number);
+console.log(typeof number);
+
+number = Infinity;
+console.log(number);
+console.log(typeof number);
+
+number = NaN;//NaN - Not a Number
+console.log(number);
+console.log(typeof number);
+
+//BOOLEAN true || false
+let isOnline = true;
+console.log(isOnline);
+console.log(typeof isOnline);
+
+isOnline = false;
+console.log(isOnline);
+console.log(typeof isOnline);
+
+//UNDEFINE
+let message;
+console.log(message);
+console.log(typeof message);
+
+//NULL
+let user = null;
+console.log(user);
+console.log(typeof user);
