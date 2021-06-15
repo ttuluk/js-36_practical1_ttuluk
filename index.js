49e0747a2a55c43e7ea14a08f3eee2bf605c5f2d
ttuluk <ttasya.k@gmail.com>
2021-06-16 00:33:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 //import "./js/variables.js";
 //import "./js/primitives.js";
-import "./js/toString.js";
+//import "./js/toString.js";
+//import "./js/toNumber.js";
+//import "./js/toBoolean.js";
+import "./js/false-values.js";
